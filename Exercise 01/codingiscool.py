66582258f977f9ed36f4dd64d9a69f729a9445c3
Arman <mohammedarman1845@gmail.com>
2024-10-26 19:20:28 +0400

    updating codingiscool.py
    Changed the mistake in comment

--- Exercise 01/codingiscool.py
+++ Exercise 01/codingiscool.py
@@ -1,10 +1,10 @@
 #Assigning the first word to the variable word1 
 word1 = "coding"
 
-#Assigning the first word to the variable word2
+#Assigning the second word to the variable word2
 word2 = "is"
 
-#Assigning the first word to the variable word3
+#Assigning the third word to the variable word3
 word3 = "cool"
 c = word1 + " " + word2 + " " + word3 
 print(c)
