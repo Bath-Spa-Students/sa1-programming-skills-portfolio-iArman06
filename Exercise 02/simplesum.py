410f0e3737fd2a1a492dbee24145e36778e4a0b6
Arman <mohammedarman1845@gmail.com>
2024-10-26 19:00:12 +0400

    simplesum.py
    Simple Sum

---
+++ Exercise 02/simplesum.py
@@ -0,0 +1,14 @@
+#Exercise 2: Simple Sums
+
+ #Adding the value to the first Variable var1
+var1 = 50
+
+ #Adding the value to second first Variable var2
+var2 = 95
+
+#Adding both the variables values 
+sum = var1 + var2  #sum is a third variable which stores the result of var1 + var2 i.e, 50 + 95
+
+#Result 
+print (f'The sum of the given values is: {sum}')
+#The result will be 50+95 which is 145
