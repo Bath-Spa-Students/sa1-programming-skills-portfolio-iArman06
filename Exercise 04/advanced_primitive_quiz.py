2576e177fbf8a707d92d7f82f27bd7bbbcf20f8f
Arman <mohammedarman1845@gmail.com>
2024-10-29 13:38:40 +0400

    advanced_primitive_quiz.py
    Advanced Requirements part

---
+++ Exercise 04/advanced_primitive_quiz.py
@@ -0,0 +1,242 @@
+#Advanced Primitive Quiz
+
+#First Question
+real_answer = "paris" #real_answer is a variable which holds the actual answer of the question
+
+answer = input ("1. What is the capital of France? : ")
+#answer is a Variable which will hold the answer given by the user
+
+#Result
+#Usage of lower() function won't affect the comparison of the user's answer to the actual answer 
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Well Done! , The answer is Correct.\n") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Ohno! , The answer is Incorrect...The Capital of France is {real_answer}.\n")
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+
+
+
+#Second Question 
+
+real_answer = "berlin" 
+answer = input ("2. What is the capital of Germany? : ")
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print (f"Well Done! , The answer is right which is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"So Sad , The answer is Incorrect...The Capital of Germany is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+   
+
+
+
+#Third Question
+
+real_answer = "dublin" 
+answer = input ("3. What is the capital of Ireland? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Well Done! , Your Answer is Correct :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Aww , The answer is Incorrect...The Capital of Ireland is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+    
+
+
+
+#Fourth Question 
+
+real_answer = "rome" 
+answer = input ("4. What is the capital of Italy? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Wow! , Your Answer is right.\n") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Ohno , The answer is Incorrect...The Capital of Italy is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+   
+
+
+
+
+#Fifth Question
+
+real_answer = "oslo" 
+answer = input ("5. What is the capital of Norway? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Congrats! , Your Answer is Correct :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Sad , The answer is Incorrect...The Capital of Norway is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+ 
+
+
+
+#Sixth Question 
+
+real_answer = "lisbon" 
+answer = input ("6. What is the capital of Portugal? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Amazing! , Your Answer is Correct :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Ohno, The answer is Incorrect...The Capital of Portugal is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+
+
+
+
+#Seventh Question
+
+real_answer = "moscow"
+answer = input ("7. What is the capital of Russia? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Amazing! , Your Answer is Correct :) .\n") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Noo, The answer is Incorrect...The Capital of Russia is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+
+
+
+#Eight Question 
+
+real_answer = "madrid" 
+answer = input ("8. What is the capital of Spain? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print (f"Congrats! , Your Answer is Correct which is {real_answer} :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"So Sorry, The answer is Incorrect...The Capital of Spain is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+    
+
+
+
+#Ninth Question 
+
+real_answer = "bern" 
+answer = input ("9. What is the capital of Switzerland? : ")
+
+
+#Result
+
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Amazing! , Your Answer is Correct :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Ohno, The answer is Incorrect...The Capital of Switzerland is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+  
+
+
+
+#Tenth Question 
+
+real_answer = "london" 
+answer = input ("10. What is the capital of United Kingdom? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print ("Well done! , Your Answer is Correct :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Sorry, The answer is Incorrect...The Capital of United Kingdom is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+    
+
+
+
+
+#Eleventh Question
+
+real_answer = "kiev"
+answer = input ("11. What is the capital of Ukraine? : ")
+
+
+#Result
+
+if answer.lower() == real_answer : 
+#We use lower() method , which will convert the answer given by user to lowercase and check with the real answer 
+
+    print (f"Congratulations! , Your Answer is {real_answer} which is 100% right :).\n ") 
+    #This statement will be printed if the answer given by user matches the variable real_answer
+
+else:
+    print (f"Ohno, The answer is Incorrect...The Capital of Ukraine is {real_answer}.\n") 
+    #This statement will be printed if the answer given by user does not matches the variable real_answer
+
+ 
+print ("Well done Genius , You have done your best. All the best for the future")
