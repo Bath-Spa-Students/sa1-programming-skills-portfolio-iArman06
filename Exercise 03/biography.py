3fa2826870ff5248ecfc59db55ecf003fe63f333
Arman <mohammedarman1845@gmail.com>
2024-11-14 00:03:46 +0400

    Exercise 3: Biography
    Exercise 3: Biography - Basic

--- Exercise 03/biography.py
+++ Exercise 03/biography.py
@@ -6,5 +6,5 @@ my_info = { "name" : "Arman",           #my_info is a variable which will hold t
            "age": 18}
 
 #result
-#we use the newline character \n to move the cursor to new line , therefore the text will start from the next line
+ #we use the newline character \n to move the cursor to new line , therefore the text will start from the next line
 print (f"Name : {my_info['name']}\nHometown : {my_info['hometown']}\nAge : {my_info['age']}")
