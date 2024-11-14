bb3c896816353521b10147b5582dbad1c4648a1d
Arman <mohammedarman1845@gmail.com>
2024-11-15 00:06:17 +0400

    Exercise 7: Some Counting
    Exercise 7: Some Counting using while loop

---
+++ Exercise 07/somecounting.py
@@ -0,0 +1,35 @@
+# Exercise 7: Some Counting
+
+#loop that counts up from 0 to 50 in increments of 1.
+print("Couting Number from 0 till 50")
+for i in range(0,51,1): #the 1 inside the bracked is used for increment by 1 from 0 till 50
+    print(i)
+   
+
+
+#loop that counts down from 50 to 0 in decrements of 1.
+print("\n")
+print("Counting down from 50 till 0")
+for j in range(50,-1,-1): #the -1 on the right inside the bracked is used for decrement by 1 from 50 till 0
+    print(j)
+
+
+# loop that counts up from 30 to 50 in increments of 1.
+print("\n")
+print("Counting up from 30 till 50")
+for i in range(30,51,1):
+    print(i)
+
+
+#loop that counts down from 50 to 10 in decrements of 2
+print("\n")
+print("Counting down from 50 till 10 with a decrement of 2") 
+for j in range(50,9,-2):
+    print(j)
+
+
+#loop that counts up from 100 to 200 in increments of 5.
+print("\n")
+print("Counting from 100 till 200 with a increment of 5") 
+for i in range(100,201,5):
+    print(i)
