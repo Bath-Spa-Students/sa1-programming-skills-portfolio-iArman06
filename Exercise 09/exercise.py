6e758526ba482482e846a8659c2961517c887537
Arman <mohammedarman1845@gmail.com>
2024-11-15 18:05:57 +0400

    Exercise 9: Hello
    Exercise 9: Hello- Fill in the blanks

---
+++ Exercise 09/exercise.py
@@ -0,0 +1,9 @@
+def hello():
+    print("Hello")  #to print "Hello" to the console
+
+def main():
+    hello()  #to call the hello() function
+
+if __name__ == "__main__":
+    main() #starts the program by calling main function
+
