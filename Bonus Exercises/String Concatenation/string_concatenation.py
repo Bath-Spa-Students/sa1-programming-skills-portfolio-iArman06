32163fbdf0bf315e94d943ad3f65cfe797d4f204
Arman <mohammedarman1845@gmail.com>
2024-11-21 00:18:36 +0400

    Bonus Exercises
    Bonus Exercise 1

---
+++ Bonus Exercises/String Concatenation/string_concatenation.py
@@ -0,0 +1,13 @@
+# Write three strings in different variables and print the output as one string.
+
+#declaring three variables and putting different strings in it
+
+strng1 = "I"
+strng2 = "love"
+strng3 = "coding"
+
+#using the addition operator to add all the strings 
+result = strng1 + " " + strng2 + " " + strng3
+
+#result
+print(result)
