296928d47b31de5c057d2a98a3da17ae840dbaaf
Arman <mohammedarman1845@gmail.com>
2024-11-21 00:19:59 +0400

    Bonus Exercises
    Bonus Exercises 4

---
+++ Bonus Exercises/List Names/listnames.py
@@ -0,0 +1,12 @@
+#Store the names of a few of your friends in a list called names. Print each person’s name
+# by accessing each element in the list, one at a time.
+
+#declaring a list 
+names = ["Raza" , "Sulaiman" , "Raza" , "Abbas"]
+
+#result 
+#now we will print each person's name by accessing each element one at a time 
+print(names[0]) #first name in the list - Raza
+print(names[1]) #second name in the list - Sulaiman
+print(names[2]) #third name in the list - Raza
+print(names[3]) #fourth name in the list - Abbas
