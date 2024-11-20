39f324842e7bc69beacc36a33fc1aea1739b2a23
Arman <mohammedarman1845@gmail.com>
2024-11-21 00:20:56 +0400

    Bonus Exercises
    Bonus Exercises 5 -  Version 1 and 2

---
+++ Bonus Exercises/Control Structures/control_structure1.py
@@ -0,0 +1,15 @@
+#Imagine an alien was just shot down in a game. Create a variable called alien_color and
+#assign it a value of 'green', 'yellow', or 'red'.
+
+#•Write an if statement to test whether the alien’s color is green. If it is, print a message
+#that the player just earned 5 points.
+
+#•Write one version of this program that passes the if test and another that fails. (The
+#version that fails will have no output.)
+
+
+#creating a variable called "alien_color" to store the color of the alien 
+alien_color = "green"
+
+if alien_color == "green": #if alien color is green then the message below will be printed
+    print("You have just earned 5 points , Congrats") #message will be printed once the program runs
---
+++ Bonus Exercises/Control Structures/control_structure2.py
@@ -0,0 +1,17 @@
+#Imagine an alien was just shot down in a game. Create a variable called alien_color and
+#assign it a value of 'green', 'yellow', or 'red'.
+
+#•Write an if statement to test whether the alien’s color is green. If it is, print a message
+#that the player just earned 5 points.
+
+#•Write one version of this program that passes the if test and another that fails. (The
+#version that fails will have no output.)
+
+
+#creating a variable called "alien_color" to store the color of the alien 
+alien_color = "yellow" #store yellow
+
+if alien_color == "green": #checking for green
+    print("You have just earned 5 points , Congrats")
+
+    #no output will be displayed as the version fails 
