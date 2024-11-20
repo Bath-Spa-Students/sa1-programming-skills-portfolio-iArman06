b46d3183fffd859334ed063a8b3386b717c45576
Arman <mohammedarman1845@gmail.com>
2024-11-21 00:19:16 +0400

    Bonus Exercises
    Bonus Exercise 2

---
+++ Bonus Exercises/Variables/variables.py
@@ -0,0 +1,15 @@
+# Assign a message to a variable, and print that message. Then change the value of the 
+# variable to a new message, and print the new message.
+print("\n") #Creating a gap
+
+#Creating a variable to store a message
+var1 = "Wow guys , She is cute"
+#printing the message
+print(var1)
+
+#Updating the message in the same variable 
+var1 = "Ohno , I was wrong"
+#printing the updated message
+print(var1)
+
+print("\n") #Creating a gap
